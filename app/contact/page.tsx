e7e2775b250aa444bfd3f18126e790caac24f60b
Raghu S <50697306+raghu-007@users.noreply.github.com>
2023-09-15 12:27:08 +0530

    update of social info in page.tsm

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/raghu7_",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@raghu7_",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:raghu-07@outlook.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "raghu-07@outlook.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/raghu-007",
 		label: "Github",
-		handle: "chronark",
+		handle: "Raghu S",
 	},
 ];
 
